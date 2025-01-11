e30178920e2c6a1d2c5d675ce5ea63574139ff75
DarekStaszczyk <156849826+DarekStaszczyk@users.noreply.github.com>
2025-01-11 14:54:58 +0100

    Add file index1.js and calc.js
    Simple calc API

---
+++ calc.js
@@ -0,0 +1,18 @@
+function calc(operation, a, b) {
+    switch (operation) {
+        case 'add':
+            return a + b;
+        case 'subtract':
+            return a - b;
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            return a / b;
+
+    }
+
+}
+
+module.exports = {
+    calc,
+};
